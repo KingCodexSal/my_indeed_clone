0cb781e5e2b6fcd98c47c1f5d033a39577798484
KingCodexSal <kingsalvy983@gmail.com>
2025-02-01 12:25:43 +0100

    15th commit

--- server-update/config/db.js
+++ server-update/config/db.js
@@ -1,12 +1,15 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true,})
-    .then(()=>{
-        console.log('DB CONNECTED SUCESSFULLY')
-    })
-    .catch ((err)=>{
-        console.log('Connection failed')
-    })
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB CONNECTED SUCESSFULLY");
+  })
+  .catch((err) => {
+    console.log("Connection failed");
+  });
